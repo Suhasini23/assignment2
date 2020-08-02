762a7265faca6ad855306b68c502aa93637adbac
suhasini23 <ssumisingyan@gmail.com>
2020-08-02 19:30:53 +0530

    Updates

---
+++ precache-manifest.ac81126a2eb1835c0b6bd663d1614989.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb1111c0079612d781d9e0d7efd17e5",
+    "url": "/assignment2/index.html"
+  },
+  {
+    "revision": "7447902150f184f09ef5",
+    "url": "/assignment2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "27e064b68eed8e5a4ce3",
+    "url": "/assignment2/static/js/2.7ea592bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assignment2/static/js/2.7ea592bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7447902150f184f09ef5",
+    "url": "/assignment2/static/js/main.3749d15a.chunk.js"
+  },
+  {
+    "revision": "5c4758c14993523d94a7",
+    "url": "/assignment2/static/js/runtime-main.3ff15eed.js"
+  },
+  {
+    "revision": "5ea8519c752eb949454227300780f23a",
+    "url": "/assignment2/static/media/active.5ea8519c.svg"
+  },
+  {
+    "revision": "42fafb33a34ef03ff042c05c043890d3",
+    "url": "/assignment2/static/media/disabledImg.42fafb33.svg"
+  }
+]);
